592a2f8cc41e5640e0f7f8eadbb531107388f816
wwc3 <98783431+wwc3@users.noreply.github.com>
2022-03-13 19:47:02 -0400

    added label style to days in forecast

--- src/script.js
+++ src/script.js
@@ -50,7 +50,7 @@ function displayForecast(response) {
     <span class="image"><center>
      <img src="http://openweathermap.org/img/wn/${
        forecastDay.weather[0].icon
-     }@2x.png" alt="" width="45" />
+     }@2x.png" alt="" width="43" />
      
      </center>
     </span>
